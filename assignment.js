a2627f000307d88bb8c09145d81a2b9c10ac4f9d
mani kanta <mainapagolla@gmail.com>
2023-10-14 09:53:43 +0530

    first commit

---
+++ assignment.js
@@ -0,0 +1,8 @@
+function password(){
+    var pass=document.getElementById("password").value;
+var repass=document.getElementById("repassword").value;
+if(pass!=repass){
+    alert("passwords doesnt match")
+}
+}
+ 
